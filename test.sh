bd2402a336a361b12e2db5d5f29a6a8b9b57e0ef
adamthedash <adamthedash2@gmail.com>
2024-12-16 00:47:06 +0000

    Add script to use ImageMagick to convert dds files to png

--- test.sh
+++ test.sh
@@ -2,5 +2,23 @@
 STEAM_FOLDER="/mnt/e/SteamLibrary/steamapps/common/Path of Exile"
 OUT_FOLDER="/mnt/e/programming/data/poe/file_dumps"
 
-cargo run --release --bin dump_paths -- "$STEAM_FOLDER" | grep "\.dat64$" | cargo run --release --bin dump_files -- "$STEAM_FOLDER" "$OUT_FOLDER"
+# Extract the dat64 files
+# cargo run --release --bin dump_paths -- "$STEAM_FOLDER" | grep "\.dat64$" | cargo run --release --bin dump_files -- "$STEAM_FOLDER" "$OUT_FOLDER"
 
+# Extract the dds & header files
+# cargo run --release --bin dump_paths -- "$STEAM_FOLDER" | grep -E "\.dds(\.header)?$" | cargo run --release --bin dump_files -- "$STEAM_FOLDER" "$OUT_FOLDER"
+
+# Use imageMagick to turn all the dds files into png
+PNG_FOLDER="/mnt/e/programming/data/poe/dds_pngs"
+find "$OUT_FOLDER" -type f -name "*.dds" | while read -r dds_path; do
+  # Figure out the right paths
+  png_path="$PNG_FOLDER/${dds_path#$OUT_FOLDER/}"
+  png_path="${png_path%.dds}.png"
+
+  # Create output folder
+  mkdir -p "$(dirname "$png_path")"
+
+  # Convert image
+  echo "$dds_path -> $png_path"
+  magick "$dds_path" "$png_path"
+done
