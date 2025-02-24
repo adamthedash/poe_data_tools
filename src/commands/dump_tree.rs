bdea78e90d2b5ffb6ac96d9dd6e897cfdfffd1ad
adamthedash <adamthedash2@gmail.com>
2025-02-24 08:38:48 +0000

    Add bones of CLI for dumping tree

--- src/commands/dump_tree.rs
+++ src/commands/dump_tree.rs
@@ -1,15 +1,66 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test() {
-        // Load PSG tree
+use std::{collections::HashMap, path::Path};
 
-        // Load DAT table
+use anyhow::{anyhow, bail, Context, Result};
 
-        // Load schema
+use super::Patch;
+use crate::{
+    bundle_fs::FS, commands::dump_tables::load_parsed_table, dat::ivy_schema::fetch_schema,
+    tree::psg::parse_psg,
+};
 
-        // Create LUT table.PassiveSkillGraphId -> table row
+pub fn dump_tree(
+    fs: &mut FS,
+    cache_dir: &Path,
+    _output_folder: &Path,
+    version: &Patch,
+) -> Result<()> {
+    let version = match version {
+        Patch::One => 1,
+        Patch::Two => 2,
+        _ => bail!("Only patch versions 1/2 supported for table extraction."),
+    };
 
-        // Output as JSON: https://github.com/grindinggear/skilltree-export/blob/master/data.json
-    }
+    // Load PSG
+    let passive_tree_bytes = fs.read("metadata/passiveskillgraph.psg")?;
+    let (_, _passive_tree) = parse_psg(&passive_tree_bytes)
+        .map_err(|e| anyhow!("Failed to parse passive skill tree: {:?}", e))?;
+
+    // Load DAT schemas
+    let schemas = fetch_schema(cache_dir)
+        .context("Failed to fetch schema file")
+        .unwrap();
+
+    // Load DAT tables
+    // ==================== Passive skills ==================
+    let passive_table = load_parsed_table(fs, &schemas, "data/passiveskills.datc64", version)?;
+    println!("{:?}", passive_table);
+
+    // Create LUT between passive skill ID and passive row
+    let col_name = "PassiveSkillGraphId";
+    let col_index = passive_table
+        .try_get_column_index(col_name)
+        .context(format!("Column not found in dat table: {:?}", col_name))?;
+    let _passive_lut = passive_table[col_index]
+        .u16()?
+        .into_iter()
+        .enumerate()
+        .fold(HashMap::new(), |mut hm, (row, passive_id)| {
+            if let Some(passive_id) = passive_id {
+                hm.entry(passive_id).insert_entry(row);
+            };
+
+            hm
+        });
+
+    //println!("{:#?}", passive_lut);
+
+    // ==================== Ascendancy ==================
+    let ascendancy_table = load_parsed_table(fs, &schemas, "data/ascendancy.datc64", version)?;
+    println!("{:?}", ascendancy_table);
+
+    // ==================== Stats ==================
+    let stats_table = load_parsed_table(fs, &schemas, "data/stats.datc64", version)?;
+    println!("{:?}", stats_table);
+
+    Ok(())
 }
