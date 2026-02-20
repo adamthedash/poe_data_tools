0443e48ffa91ef4d7f2eb31d3dbf024519591f86
Adam Power <adamthedash2@gmail.com>
2026-02-20 12:45:48 +0000

    Winnow impl for ecf

--- src/file_parsers/ecf/mod.rs
+++ src/file_parsers/ecf/mod.rs
@@ -4,8 +4,9 @@ use crate::file_parsers::{FileParser, shared::utf16_bom_to_string};
 
 pub mod parser;
 pub mod types;
-use parser::parse_ecf_str;
+pub mod winnow_parser;
 use types::*;
+use winnow_parser::parse_ecf_str;
 
 pub struct ECFParser;
 
---
+++ src/file_parsers/ecf/winnow_parser.rs
@@ -0,0 +1,46 @@
+use anyhow::{Result, anyhow};
+use winnow::{
+    Parser,
+    ascii::space1,
+    combinator::{opt, preceded, repeat},
+};
+
+use super::types::*;
+use crate::file_parsers::{
+    lift_winnow::lift,
+    shared::winnow::{
+        TraceHelper, WinnowParser, parse_bool, quoted_str, separated_array, version_line,
+    },
+};
+
+fn combination<'a>() -> impl WinnowParser<&'a str, EcfCombination> {
+    (
+        separated_array(space1, quoted_str),
+        opt(preceded(space1, parse_bool)),
+    )
+        .map(|(et_files, bool1)| EcfCombination { et_files, bool1 })
+        .trace("combination")
+}
+
+pub fn parse_ecf_str(contents: &str) -> Result<EcfFile> {
+    let lines = contents
+        .lines()
+        .map(|l| l.trim())
+        .filter(|l| !l.is_empty() && !l.starts_with("//"))
+        .collect::<Vec<_>>();
+
+    let mut parser = (
+        lift(version_line()), //
+        repeat(0.., lift(combination())),
+    )
+        .map(|(version, combinations)| EcfFile {
+            version,
+            combinations,
+        });
+
+    let ecf_file = parser
+        .parse(lines.as_slice())
+        .map_err(|e| anyhow!("Failed to parse file: {e:?}"))?;
+
+    Ok(ecf_file)
+}
