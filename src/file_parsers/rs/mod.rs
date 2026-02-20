92e0f45cd4fbbb7d10c99ef870683399bfacd063
Adam Power <adamthedash2@gmail.com>
2026-02-20 17:07:23 +0000

    Winnow parser for .rs files. Restrict arm filenames

--- src/file_parsers/rs/mod.rs
+++ src/file_parsers/rs/mod.rs
@@ -1,9 +1,10 @@
-pub mod parser;
+pub mod nom_parser;
 pub mod types;
+pub mod winnow_parser;
 
 use anyhow::Result;
-use parser::parse_rs_str;
 use types::*;
+use winnow_parser::parse_rs_str;
 
 use crate::file_parsers::{FileParser, shared::utf16_bom_to_string};
 
