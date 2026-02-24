701932b266becd64f70ae65a09e951672dc67300
Adam Power <adamthedash2@gmail.com>
2026-02-24 16:07:38 +0000

    Update DLP with poe2 testing

--- src/file_parsers/dlp/parser.rs
+++ src/file_parsers/dlp/parser.rs
@@ -2,32 +2,85 @@ use anyhow::{Result, anyhow};
 use winnow::{
     Parser,
     ascii::{dec_uint, float, space1},
-    combinator::{alt, delimited, opt, preceded as P, repeat, separated_pair},
-    token::literal,
+    combinator::{
+        alt, delimited, dispatch, empty, eof, opt, preceded as P, repeat, separated_pair,
+        terminated,
+    },
+    token::{literal, rest},
 };
 
 use super::types::*;
 use crate::file_parsers::{
-    lift_winnow::lift,
-    shared::winnow::{TraceHelper, WinnowParser, filename, parse_bool, quoted, version_line},
+    lift_winnow::{SliceParser, lift},
+    shared::winnow::{
+        TraceHelper, WinnowParser, filename, parse_bool, quoted, unquoted, version_line,
+    },
 };
 
-fn num_line<'a>() -> impl WinnowParser<&'a str, Nums> {
+fn headers_v2<'a>() -> impl WinnowParser<&'a str, HeadersV2> {
     (
         float, //
         P(space1, float),
         P(space1, parse_bool),
         P(space1, parse_bool),
         opt(P(space1, dec_uint)),
+        opt(P(space1, dec_uint)),
+        opt(P(space1, dec_uint)),
+        opt(P(space1, float)),
+        opt(P(space1, dec_uint)),
+        opt(P(space1, float)),
     )
-        .map(|(float1, float2, bool1, bool2, uint)| Nums {
-            float1,
-            float2,
-            bool1,
-            bool2,
-            uint,
-        })
-        .trace("num_line")
+        .map(
+            |(
+                scale_min,
+                scale_max,
+                allow_waving,
+                allow_on_blocking,
+                max_rotation,
+                uint1,
+                uint2,
+                float1,
+                audio_type,
+                float2,
+            )| HeadersV2 {
+                scale_min,
+                scale_max,
+                allow_waving,
+                allow_on_blocking,
+                max_rotation,
+                uint1,
+                uint2,
+                float1,
+                audio_type,
+                float2,
+            },
+        )
+        .trace("headers_v2")
+}
+
+fn headers_v3<'a>() -> impl SliceParser<'a, &'a str, HeadersV3> {
+    repeat(
+        0..,
+        lift(P(
+            (literal('-'), space1),
+            dispatch! {
+                terminated(unquoted(), opt(space1));
+                "RandomScale" => separated_pair(float, space1, float).map(|(min, max)| Header::RandomScale { min, max }),
+                "AllowWaving" => eof.value(Header::AllowWaving),
+                "AllowOnBlocking" => eof.value(Header::AllowOnBlocking),
+                "MaxRotation" => dec_uint.map(Header::MaxRotation),
+                "MinEdgeScale" => float.map(Header::MinEdgeScale),
+                "AudioType" => dec_uint.map(Header::AudioType),
+                "DelayMultiplier" => float.map(Header::DelayMultiplier),
+                "TimeMultiplier" => float.map(Header::TimeMultiplier),
+                "SizeMultiplier" => float.map(Header::SizeMultiplier),
+                "Seed" => dec_uint.map(Header::Seed),
+                key => rest.map(move |rest: &str| Header::Other { key: key.to_string(), rest: rest.to_string() }),
+            },
+        )),
+    )
+    .map(HeadersV3)
+    .trace("headers_v3")
 }
 
 fn entry<'a>() -> impl WinnowParser<&'a str, Entry> {
@@ -67,20 +120,28 @@ pub fn parse_dlp_str(contents: &str) -> Result<DLPFile> {
         .map(|l| l.trim())
         .filter(|l| !l.is_empty())
         .collect::<Vec<_>>();
+    let mut lines = lines.as_slice();
+
+    let version = opt(lift(version_line()))
+        .parse_next(&mut lines)
+        .map_err(|e| anyhow!("Failed to parse file: {e:?}"))?;
 
     let mut parser = (
-        opt(lift(version_line())),
-        lift(num_line()), //
+        dispatch! {
+            empty.value(version.unwrap_or(0));
+            ..3 => lift(headers_v2()).map(Headers::V2),
+            3.. => headers_v3().map(Headers::V3),
+        },
         repeat(0.., lift(entry())),
     )
-        .map(|(version, nums, entries)| DLPFile {
+        .map(|(headers, entries)| DLPFile {
             version,
-            nums,
+            headers,
             entries,
         });
 
     let pet_file = parser
-        .parse(lines.as_slice())
+        .parse(lines)
         .map_err(|e| anyhow!("Failed to parse file: {e:?}"))?;
 
     Ok(pet_file)
--- src/file_parsers/dlp/types.rs
+++ src/file_parsers/dlp/types.rs
@@ -7,18 +7,48 @@ pub struct Entry {
     pub points: Vec<(f32, f32)>,
 }
 
+#[derive(Debug, Serialize, Clone)]
+#[serde(tag = "kind", content = "data")]
+pub enum Header {
+    RandomScale { min: f32, max: f32 },
+    AllowWaving,
+    AllowOnBlocking,
+    MaxRotation(u32),
+    MinEdgeScale(f32),
+    AudioType(u32),
+    DelayMultiplier(f32),
+    SizeMultiplier(f32),
+    TimeMultiplier(f32),
+    Seed(u32),
+    Other { key: String, rest: String },
+}
+
 #[derive(Debug, Serialize)]
-pub struct Nums {
-    pub float1: f32,
-    pub float2: f32,
-    pub bool1: bool,
-    pub bool2: bool,
-    pub uint: Option<u32>,
+pub struct HeadersV3(pub Vec<Header>);
+
+#[derive(Debug, Serialize)]
+pub struct HeadersV2 {
+    pub scale_min: f32,
+    pub scale_max: f32,
+    pub allow_waving: bool,
+    pub allow_on_blocking: bool,
+    pub max_rotation: Option<u32>,
+    pub uint1: Option<u32>,
+    pub uint2: Option<u32>,
+    pub float1: Option<f32>,
+    pub audio_type: Option<u32>,
+    pub float2: Option<f32>,
+}
+
+#[derive(Debug, Serialize)]
+pub enum Headers {
+    V2(HeadersV2),
+    V3(HeadersV3),
 }
 
 #[derive(Debug, Serialize)]
 pub struct DLPFile {
     pub version: Option<u32>,
-    pub nums: Nums,
+    pub headers: Headers,
     pub entries: Vec<Entry>,
 }
