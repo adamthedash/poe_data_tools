0111027ad99cdacd93e7eb1dd3e25d4784a77dab
Adam Power <adamthedash2@proton.me>
2026-06-16 23:52:42 +0100

    Update steam example

--- crates/poe_data_tools-lib/examples/steam.rs
+++ crates/poe_data_tools-lib/examples/steam.rs
@@ -7,8 +7,9 @@ use poe_data_tools::{
 };
 
 fn main() {
+    // (I don't have a steam install, but it's the same stucture as the CDN cache, so I'm just using
+    // that)
     let steam_folder =
-        // Path::new("~/.cache/poe_data_tools/patch-poe2.poecdn.com/4.3.1.2").to_owned();
         Path::new("/home/adam/.cache/poe_data_tools/patch-poe2.poecdn.com/4.3.1.2").to_owned();
     let fs = FS::from_steam(steam_folder)
         .context("cound't create file system")
@@ -20,11 +21,10 @@ fn main() {
         .filter(|f| f.ends_with(".psg"))
         .collect::<Vec<_>>();
     for (file, bytes) in fs.batch_read(&tree_files) {
-        // let Ok(bytes) = bytes else {
-        //     eprintln!("Error reading {file:?}");
-        //     continue;
-        // };
-        let bytes = bytes.context("blah").unwrap();
+        let Ok(bytes) = bytes else {
+            eprintln!("Error reading {file:?}");
+            continue;
+        };
 
         let parser = PSGParser { version: 2 };
         let Ok(tree) = parser.parse(&bytes).inner else {
