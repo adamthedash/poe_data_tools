173d740981053da7de24676ab1e4b420a45c2c5b
Adam Power <adamthedash2@proton.me>
2026-06-30 16:52:55 +0100

    thiserror for .tgt (annotated_parser impl)

--- crates/poe_data_tools-lib/src/file_parsers/error.rs
+++ crates/poe_data_tools-lib/src/file_parsers/error.rs
@@ -1,3 +1,7 @@
+use std::fmt::Display;
+
+use annotated_parser::{Annotation, AnnotationResult};
+
 use crate::file_parsers::shared::BOMError;
 
 pub type Result<T, E = ParseError> = std::result::Result<T, E>;
@@ -11,6 +15,15 @@ pub struct ParseError {
     inner: ParseErrorInner,
 }
 
+impl From<ParseErrorInner> for ParseError {
+    fn from(value: ParseErrorInner) -> Self {
+        Self {
+            version: None,
+            inner: value,
+        }
+    }
+}
+
 #[derive(thiserror::Error, Debug)]
 pub(crate) enum ParseErrorInner {
     /// Errors from winnow parsers
@@ -19,9 +32,8 @@ pub(crate) enum ParseErrorInner {
     Winnow(#[from] winnow::error::ContextError),
 
     /// Errors from annotated_parser
-    // TODO: Transform into proper error type
     #[error(transparent)]
-    Annotated(annotated_parser::Annotation),
+    Annotated(#[from] AnnotatedError),
 
     /// Error from anything before a parser is applied
     #[error("error during file pre-processing: {0}")]
@@ -38,6 +50,12 @@ where
 {
     fn from(value: winnow::error::ParseError<I, E>) -> Self {
         value.into_inner().into()
+    }
+}
+
+impl From<annotated_parser::Annotation> for ParseErrorInner {
+    fn from(value: annotated_parser::Annotation) -> Self {
+        AnnotatedError::from(value).into()
     }
 }
 
@@ -85,5 +103,64 @@ impl<T> ParseResultEx for Result<T, ParseError> {
 
     fn with_maybe_version(self, version: Option<u32>) -> Self {
         self.map_err(|e| ParseError { version, ..e })
+    }
+}
+
+// ==============================================================
+// TODO: This goes in annotated_parser
+
+#[derive(Debug, thiserror::Error)]
+pub(crate) struct AnnotatedError {
+    /// Materialsed parser ID
+    parser_id: String,
+    /// One of failure cases
+    result: AnnotationResult,
+
+    source: Option<Box<Self>>,
+}
+
+impl Display for AnnotatedError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.result {
+            AnnotationResult::Success { .. } => {
+                unreachable!("annotated errors cannot contain successful results")
+            }
+            AnnotationResult::Incomplete { start } => {
+                write!(
+                    f,
+                    "[incomplete] @ {start} \"extra data expected\" {}",
+                    self.parser_id
+                )
+            }
+            AnnotationResult::Child { start } => write!(f, "[child] @ {start} {}", self.parser_id),
+            AnnotationResult::Invalid { span, reason } => {
+                write!(f, "[invalid] @ {span:?} {reason:?} {}", self.parser_id)
+            }
+        }
+    }
+}
+
+impl From<Annotation> for AnnotatedError {
+    fn from(mut value: Annotation) -> Self {
+        // TODO: A better error display for annotation trees so we don't need to do this
+        value = value.to_failure_tree().expect("on error path");
+        value.materialize();
+
+        let mut errors = value.failure_path().into_iter();
+
+        // Prep bottom of stack
+        let first = errors.by_ref().next().expect("must be at least 1 error");
+        let first = Self {
+            parser_id: first.parser_id,
+            result: first.result,
+            source: None,
+        };
+
+        // Fold rest of stack in
+        errors.fold(first, |source, anno| Self {
+            parser_id: anno.parser_id,
+            result: anno.result,
+            source: Some(Box::new(source)),
+        })
     }
 }
--- crates/poe_data_tools-lib/src/file_parsers/tgt/mod.rs
+++ crates/poe_data_tools-lib/src/file_parsers/tgt/mod.rs
@@ -1,18 +1,21 @@
-use crate::file_parsers::{FileParser, VersionedResult};
+use crate::file_parsers::{
+    FileParser2,
+    error::{ParseErrorInner, Result},
+};
 
 pub mod parser;
 pub mod types;
-use anyhow::Context;
 use parser::parse_tgt_str;
 use types::*;
 
 pub struct TGTParser;
 
-impl FileParser for TGTParser {
+impl FileParser2 for TGTParser {
     type Output = TGTFile;
 
-    fn parse(&self, bytes: &[u8]) -> VersionedResult<Self::Output> {
-        let contents = String::from_utf16le(bytes).context("Failed to parse file as UTF16LE")?;
+    fn parse(&self, bytes: &[u8]) -> Result<Self::Output> {
+        let contents =
+            String::from_utf16le(bytes).map_err(|e| ParseErrorInner::Preprocessing(e.into()))?;
 
         parse_tgt_str(&contents)
     }
