bc467f54d9f24522b49d8ad0b25245ea7ab3e9e8
Adam Power <adamthedash2@proton.me>
2026-06-30 23:51:48 +0100

    Remove annotated parser anyhow conversion

--- crates/poe_data_tools-lib/src/file_parsers/shared/annotated_parser.rs
+++ crates/poe_data_tools-lib/src/file_parsers/shared/annotated_parser.rs
@@ -1,6 +1,4 @@
-use annotated_parser::{
-    AnnotationReturn, ParseWithResult, combinators::TakeTillExc, parsers::TakeArray, prelude::*,
-};
+use annotated_parser::{combinators::TakeTillExc, parsers::TakeArray, prelude::*};
 
 /// Simpler trait alias for byte parsers
 pub trait U8Parser: for<'a> Parser<&'a [u8]> {}
@@ -32,38 +30,4 @@ pub fn quoted() -> impl StrParser<Output = String> {
     TakeTillExc::new("\"")
         .surrounded_by_sym("\"")
         .trace_opaque("quoted")
-}
-
-/// Helper function to convert annotated errors into anyhow with proper context
-fn anno_to_anyhow(annotation: AnnotationReturn) -> anyhow::Error {
-    let AnnotationReturn::Annotated(mut annotation) = annotation else {
-        return annotation.into();
-    };
-    annotation = annotation.to_failure_tree().expect("error path");
-    annotation.materialize();
-
-    let mut stack = annotation.failure_path().into_iter();
-
-    let first = stack
-        .next()
-        .expect("If annotation is a failure then there should be at least 1 entry");
-
-    stack.fold(anyhow::Error::from(first), |err, anno| err.context(anno))
-}
-
-pub trait ToAnyhow {
-    type Value;
-
-    fn to_anyhow(self) -> anyhow::Result<Self::Value>;
-}
-
-impl<T> ToAnyhow for ParseWithResult<T> {
-    type Value = T;
-
-    fn to_anyhow(self) -> anyhow::Result<Self::Value> {
-        match self {
-            Ok((v, _)) => Ok(v),
-            Err(a) => Err(anno_to_anyhow(a)),
-        }
-    }
 }
