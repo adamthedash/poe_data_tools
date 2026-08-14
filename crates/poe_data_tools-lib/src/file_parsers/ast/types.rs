a94562391307ca8f26b30a84dc8b31251c2baa41
Adam Power <adamthedash2@proton.me>
2026-08-14 09:57:03 +0100

    Update AST parser. Now passing on all poe 1/2 files

--- crates/poe_data_tools-lib/src/file_parsers/ast/types.rs
+++ crates/poe_data_tools-lib/src/file_parsers/ast/types.rs
@@ -9,7 +9,7 @@ pub struct ASTFile {
     pub bones: Vec<Bone>,
     pub lights: Vec<Light>,
     pub animations: Vec<Animation>,
-    pub bundle: BundleFile,
+    pub bundle: Option<BundleFile>,
 }
 
 #[derive(Clone, Debug, Serialize)]
@@ -17,8 +17,7 @@ pub struct Header {
     pub version: u8,
     pub num_bones: u8,
     pub unk1: u8,
-    pub num_animations: u8,
-    pub unk2: u8,
+    pub num_animations: u16,
     pub unk3: u8,
     pub unk4: u8,
     pub num_lights: u8,
@@ -26,11 +25,11 @@ pub struct Header {
 
 #[derive(Clone, Debug, Serialize)]
 pub struct Bone {
-    pub sibling: u8,
-    pub child: u8,
+    pub sibling: Option<u8>,
+    pub child: Option<u8>,
     pub transform: [[f32; 4]; 4],
     pub name_length: u8,
-    pub unk1: u8,
+    pub unk1: Option<u8>,
     pub name: String,
 }
 
@@ -39,9 +38,16 @@ pub struct Bone {
 pub struct Light {
     pub name_length: u8,
     #[serde_as(as = "[_; _]")]
-    pub unk_bytes1: [u8; 55],
+    pub unk_bytes1: [u8; 51],
     pub unk_bytes2: Option<[u8; 4]>,
+    pub unk_bytes3: Option<[u8; 4]>,
     pub name: String,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct DataLocation {
+    pub offset: u32,
+    pub length: u32,
 }
 
 #[derive(Clone, Debug, Serialize)]
@@ -53,8 +59,33 @@ pub struct Animation {
     pub unk3: Option<u8>,
     pub name_length: u8,
     pub parent_name_length: Option<u8>,
-    pub data_offset: u32,
-    pub data_size: u32,
+    pub data_location: Option<DataLocation>,
     pub name: String,
     pub parent_name: Option<String>,
+    pub data: Option<Vec<Track>>,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct TrackHeader {
+    pub unk1: Option<u8>,
+    pub index: u32,
+    pub num_scales: u32,
+    pub num_rotations: u32,
+    pub num_positions: u32,
+    pub num_unk2: u32,
+    pub num_unk3: u32,
+    pub num_unk4: u32,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct Track {
+    pub header: TrackHeader,
+    pub unk1: Option<[u8; 8]>,
+
+    pub scales: Vec<[f32; 4]>,
+    pub rotations: Vec<[f32; 5]>,
+    pub positions: Vec<[f32; 4]>,
+    pub unk2s: Vec<[f32; 4]>,
+    pub unk3s: Vec<[f32; 5]>,
+    pub unk4s: Vec<[f32; 4]>,
 }
